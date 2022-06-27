17154a1340737e71789ba87ddeb93c006031f556
VitKad <74490554+VitKad@users.noreply.github.com>
2022-06-27 21:18:53 +0300

    add comments

--- Barcode_scanner.py
+++ Barcode_scanner.py
@@ -1,85 +1,58 @@
 from pyzbar import pyzbar
-import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from glob import glob
 
-
-def decode(image):
-    # decodes all barcodes from an image
-    decoded_objects = pyzbar.decode(image)
+# функция обнаружения штрихкодов
+def find(image):
+    # расшифровка штрихкодов с изображения
+    codes = pyzbar.decode(image)
     numCode = 0
-    for obj in decoded_objects:
-        # draw the barcode
-        #print(f"Обнаружен штрих-код:\n{obj}")
+    for code in codes:
         numCode = numCode + 1
-        #my_file = open("D:/QR-scanner/QR-scanner/coordinate.txt", "a+")
-        my_file.write(f"Координаты штрихкода №{numCode}\n")
-        #my_file.close()		
-        image = draw_barcode(obj, image)
-
-        # print barcode type & data
-        print("Тип кода:", obj.type)
-        print("Данные:\n", obj.data.decode())
+        # запись номера штрихкода в файл
+        coordFile.write(f"Координаты штрихкода №{numCode}\n")
+        # выделение	штрихкода на изображении	
+        image = select(code, image)
+        print("Данные:\n", code.data.decode())
         print()
     numCode = 0
     return image
 
 
-def draw_barcode(decoded, image):
-    n_points = len(decoded.polygon)
-    for i in range(n_points):
-        image = cv2.line(image, decoded.polygon[i], decoded.polygon[(i+1) % n_points], color=(255, 0, 0), thickness=5)
-        print(decoded.polygon[i].x)
-        
-        #my_file = open("D:/QR-scanner/QR-scanner/coordinate.txt", "a+")
-        my_file.write(f"x={str(decoded.polygon[i].x)}, y={str(decoded.polygon[i].y)}\n")
-        #my_file.close()
-		
-        #cv2.show()
-    # раскомментируйте выше и закомментируйте ниже, если хотите нарисовать многоугольник, а не прямоугольник
-    #image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top), 
-    #                        (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
-    #                        color=(0, 255, 0),
-    #                        thickness=5)
+# функция выделения штрихкода на изображении
+def select(code, image):
+    length = len(code.polygon)
+    for i in range(length):
+        # отрисовка линий между углами штрихкода
+        image = cv2.line(image, code.polygon[i], code.polygon[(i+1) % length], color=(255, 0, 0), thickness=5)
+        print(code.polygon[i].x)
+        # запись координат в файл
+        coordFile.write(f"x={str(code.polygon[i].x)}, y={str(code.polygon[i].y)}\n")
+      
     return image
 
-	
-def qr_reader(img):
-    detector = cv2.QRCodeDetector()
-    data, bbox, _ = detector.detectAndDecode(img)
-    if data:
-        print(data)
-    else:
-        print('Ничего не нашлось!')
-
-count = 0
 
 if __name__ == "__main__":
-    from glob import glob
-    my_file = open("results2/coord.txt", "w+")
-    my_file.write("Координаты распознаных штрихкодов:\n")
-    my_file.close()
-    barcodes = glob("images/1/*.jpg")
-    for barcode_file in barcodes:
+
+    count = 0
+    coordFile = open("coordinates/coord.txt", "w+")
+    coordFile.write("Координаты распознанных штрихкодов:\n")
+    coordFile.close()
+    files = glob("images/*.jpg")
+    for file in files:
         count = count + 1 
-        my_file = open("results2/coord.txt", "a+")
-        my_file.write(f"\nИзображение №{count}:\n")
-        my_file.write("----------------------------------\n")
-        img = cv2.imread(barcode_file)
-        # декодировать обнаруженные штрих-коды и получить изображение
-        # нарисованный
-        #kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-        #im = cv2.filter2D(img, -1, kernel)
-		
-        img = decode(img)
-        #width = 100
-        #height = 100
-        #dsize=(width, height)
-        #outpad = cv2.resize(img,dsize)
-        # показать изображение
+        coordFile = open("coordinates/coord.txt", "a+")
+        coordFile.write(f"\nИзображение №{count}:\n")
+        coordFile.write("----------------------------------\n")
+        # преобразование изображения в массив значений
+        img = cv2.imread(file)
+        img = find(img)
+        
+        # отображение результата на экран
         plt.imshow(img)
         plt.show()
-        my_file.write("----------------------------------\n")
-        my_file.close()   
-        cv2.imwrite(f'results2/first{count}.jpg', img)
+        coordFile.write("----------------------------------\n")
+        coordFile.close()   
+        cv2.imwrite(f'result/{count}.jpg', img)
         cv2.waitKey(0)
--- QR_scanner.py
+++ QR_scanner.py
@@ -1,15 +1,10 @@
 from pyzbar import pyzbar
 import numpy as np
 import cv2
-from PIL import Image, ImageEnhance
-
 from matplotlib import pyplot as plt
 import skimage
-from skimage import util, exposure, io, measure, feature
-from scipy import ndimage as ndi
-import numpy as np
+from skimage import exposure, io, measure, feature
 from glob import glob
-
 
 
 def find(image):
@@ -27,65 +22,70 @@ def select(code, image):
 		image = cv2.line(image, code.polygon[i], code.polygon[(i+1) % length], color=(255, 0, 0), thickness=5)
 	return image
 
-count = 0	
 		
 if __name__ == "__main__":
-	files = glob("images/1/6.jpg")
+	count = 0	
+	files = glob("images/*.jpg")
 	for file in files:
-	
 		img = plt.imread(file)
+		# преобразование в черно-белый цвет
 		image = skimage.color.rgb2gray(img)
 		plt.imshow(image, cmap='gray')
 		plt.show()
+		# коррекция гаммы
 		gamma = exposure.adjust_gamma(image, 5)
 		plt.imshow(gamma, cmap='gray')
 		plt.show()
+		# нормализация изображения
 		thr = (gamma >= 0.3)
 		plt.imshow(thr, cmap='gray')
 		plt.show()
+		# обнаружение границ
 		edges = feature.canny(thr, sigma=10)
 		plt.imshow(edges)
 		plt.show()
+		# поиск контуров
 		contours = measure.find_contours(edges, 0, fully_connected='high', positive_orientation='high')
 		
-		
-		my_file = open("results1/coord.txt", "w+")
-		my_file.write("Координаты распознаных штрихкодов:\n")
-		#fig, (ax2) = plt.subplots(ncols=1, figsize=(9, 4))
+		coordFile = open("coordinates/coord.txt", "w+")
+		coordFile.write("Координаты распознаных штрихкодов:\n")
 		plt.imshow(edges)
 
-		
+		# отображение всех найденных контуров на изображение
 		for contour in contours:
-			#print(contour)
 			plt.plot(contour[:,1], contour[:,0], linewidth=2, color = (0, 0, 1))
-
+		
 		io.imshow(img)
 		plt.show()
-		
+
+		# отображение всех контуров после аппроксимации
 		for contour in contours:
-			
 			coords = measure.approximate_polygon(contour, tolerance=20)
 			plt.plot(coords[:, 1], coords[:, 0], '-r', linewidth=2)
-			print(f"Результат усреднения: из {len(contour)} заданных точек получилось {len(coords)}")
-			my_file.write(str(coords))
+			coordFile.write(str(coords))
 	
-		my_file.close()
+		coordFile.close()
+	  
+		# поиск точек экстремума
 		positions = np.concatenate(contours, axis=0)
 		min_pos_x = int(min(positions[:,1]))
 		max_pos_x = int(max(positions[:,1]))
 		min_pos_y = int(min(positions[:,0]))
 		max_pos_y = int(max(positions[:,0]))
 
+		# координаты крайних точек
 		start = (min_pos_x, min_pos_y)
 		end = (max_pos_x, max_pos_y)
 		cv2.rectangle(img, start, end, (255, 0, 0), 5)
 		io.imshow(img)
+
+		# создание нового изображения, где остается только фрагмент со штрихкодами
+		newImage=img[min_pos_y:max_pos_y, min_pos_x:max_pos_x]
+		io.imshow(newImage)
 		plt.show()
-		new_img=img[min_pos_y:max_pos_y, min_pos_x:max_pos_x]
-		io.imshow(new_img)
-		plt.show()
-		img = find(new_img)
+		# поиск штрихкодов
+		img = find(newImage)
 		count = count + 1
-		cv2.imwrite(f'results1/first{count}.jpg', img)
+		cv2.imwrite(f'result/{count}.jpg', img)
 
 cv2.waitKey(0)
